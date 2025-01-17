8c037f03a1cf82bd1407fb88bba0872b4d76f824
Todd Behrmann <todd.behrmann@ensono.com>
2025-01-17 16:26:58 -0600

    add resource

--- main.tf
+++ main.tf
@@ -8,3 +8,8 @@ resource "google_compute_router" "testrouter1" {
   network = google_compute_network.vpc_network.name
   region = var.region
 }
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "local"
+  auto_create_subnetworks = false
+}
