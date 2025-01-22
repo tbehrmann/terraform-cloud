98d639af7a33dc489a1e9442c891eee2239fd0b0
Todd Behrmann <todd.behrmann@ensono.com>
2025-01-22 14:52:50 -0600

    test router3 added

--- main.tf
+++ main.tf
@@ -19,3 +19,9 @@ resource "google_compute_network" "vpc_network" {
   name                    = "local"
   auto_create_subnetworks = false
 }
+
+resource "google_compute_router" "testrouter1" {
+  name = "testrouter3"
+  network = google_compute_network.vpc_network.name
+  region = var.region
+}
