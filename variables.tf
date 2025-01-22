82e632f969d1ed23e7da32101126279c97c3719d
Todd Behrmann <todd.behrmann@ensono.com>
2025-01-22 14:14:18 -0600

    unused variable removed

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "instance_type" {
-
-    type        = string
-
-    description = "The EC2 instance type to launch"
-
-    default     = "t2.micro"
-
-    }
-
     variable "region" {
         description = "region"
     }
