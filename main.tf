2a15583e0a5a7cb94552995b32f53b09f822ee61
Todd Behrmann <todd.behrmann@ensono.com>
2025-01-23 14:04:03 -0600

    resource updated

--- main.tf
+++ main.tf
@@ -39,8 +39,3 @@ resource "google_compute_router" "testrouter1" {
 }
  
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "local"
-  auto_create_subnetworks = false
-}
-
