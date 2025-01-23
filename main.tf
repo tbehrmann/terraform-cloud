5c5092b306b4f7ccc38b277688e52b800ff16f97
Todd Behrmann <todd.behrmann@ensono.com>
2025-01-23 12:42:01 -0600

    compute instance added

--- main.tf
+++ main.tf
@@ -2,8 +2,27 @@ Provider configuration
 
 provider "gcp" {
   project = "terraform-cloud"
-  region = "us-east-1"
+  region = "us-central1"
+  zone    = "us-central1-c"
 
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
 }
 
 
